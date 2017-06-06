65e3274f5e9c02a49ff9c0c777b82857a00d6740
Alan Vincent <alan.d.vincent@gmail.com>
2017-06-06 15:11:59 -0600

    added core controllers

---
+++ app/controllers/patients_controller.rb
@@ -0,0 +1,10 @@
+class PatientsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
+  get 'appointments/new'
+
+  get 'patients/index'
+
+  get 'patients/new'
+
+  get 'patients/edit'
+
+  get 'doctors/index'
+
+  get 'doctors/show'
+
+  get 'doctors/new'
+
+  get 'doctors/edit'
+
 root 'doctors#index'
 
 resources :doctors
